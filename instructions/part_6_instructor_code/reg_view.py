712e2038c7ddbb519903f83d84dde376db0ccd95
Louis Sobel <sobel@lsmbp.local>
2012-07-13 15:09:11 +0000

    Part 6 instructions and skeleton code

---
+++ instructions/part_6_instructor_code/reg_view.py
@@ -0,0 +1,31 @@
+"""
+Code that should be copy and pasted in to
+reg/views.py to as a skeleton for creating
+the authentication views
+"""
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+@csrf_exempt
+def do_login(request):
+    if request.method == 'POST':
+        #YOUR CODE HERE
+        pass
+    
+    form = LoginForm()
+    return render_to_response('reg/login.html', {
+        'form': form,
+        'logged_in': request.user.is_authenticated()
+    })
+
+@csrf_exempt
+def do_logout(request):
+    logout(request)
+    return render_to_response('reg/logout.html')
